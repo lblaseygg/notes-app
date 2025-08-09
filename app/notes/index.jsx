a09f692426386a1807d9942bfc0daf0ded33e32d
Luis Feliciano <justblasey@gmail.com>
2025-08-08 23:43:45 -0400

    Added notes folders

---
+++ app/notes/index.jsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from "react-native";
+
+
+const NoteScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Notes</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#fff',
+    },
+});
+
+export default NoteScreen;
